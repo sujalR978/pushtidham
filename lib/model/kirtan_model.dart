124df5097dba8cb719f3cbab2f1cde66c200c2b7
sujalR978 <srashiya955@rku.ac.in>
2026-07-21 00:31:59 +0530

    #

---
+++ lib/model/kirtan_model.dart
@@ -0,0 +1,13 @@
+class KirtanModel {
+  final String id;
+  final String number;
+  final String title;
+  final String imageAsset;
+
+  KirtanModel({
+    required this.id,
+    required this.number,
+    required this.title,
+    required this.imageAsset,
+  });
+}
