f56f85d5837834c1ad89ff613f361f9aff3039b3
sujalR978 <srashiya955@rku.ac.in>
2026-07-21 18:57:06 +0530

    #

---
+++ lib/model/varta_model.dart
@@ -0,0 +1,21 @@
+class VartaModel {
+  final String id;
+  final String number;
+  final String titleGujarati;
+  final String titleBraj;
+  final String shloka;
+  final String arth;
+  final String vartaContent;
+  final String saar;
+
+  VartaModel({
+    required this.id,
+    required this.number,
+    required this.titleGujarati,
+    required this.titleBraj,
+    required this.shloka,
+    required this.arth,
+    required this.vartaContent,
+    required this.saar,
+  });
+}
