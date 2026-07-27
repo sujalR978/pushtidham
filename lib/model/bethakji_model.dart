cc00ee4305376f6c6952aee1e55c53ed9c3fd320
sujalR978 <srashiya955@rku.ac.in>
2026-07-27 23:03:29 +0530

    #

--- lib/model/bethakji_model.dart
+++ lib/model/bethakji_model.dart
@@ -1,22 +1,60 @@
+import 'dart:convert';
 
+class BethakjiModel {
+  final String id;
+  final String number;
+  final String name;
+  final String address;
+  final List<Map<String, String>> contacts;
+  final String mahatmy;
+  final String directions;
+  final List<String> rules;
 
-class Bethakji {
-  final int? id;
-  final int bethakNo;
-  final String title;
-  final int isFavorite;
-  Bethakji({
-    this.id,
-    required this.bethakNo,
-    required this.title,
-    this.isFavorite = 0,
+  BethakjiModel({
+    required this.id,
+    required this.number,
+    required this.name,
+    required this.address,
+    required this.contacts,
+    required this.mahatmy,
+    required this.directions,
+    required this.rules,
   });
-  Map<String, dynamic> tomap() {
+
+  factory BethakjiModel.fromMap(Map<String, dynamic> map) {
+    return BethakjiModel(
+      id: map['id'].toString(),
+      number: map['number'] ?? '',
+      name: map['name'] ?? '',
+      address: map['address'] ?? '',
+      // Decode JSON string back to List<Map<String, String>>
+      contacts: map['contacts'] != null
+          ? List<Map<String, String>>.from(
+              (jsonDecode(map['contacts']) as List).map(
+                (item) => Map<String, String>.from(item),
+              ),
+            )
+          : [],
+      mahatmy: map['mahatmy'] ?? '',
+      directions: map['directions'] ?? '',
+      // Decode JSON string back to List<String>
+      rules: map['rules'] != null
+          ? List<String>.from(jsonDecode(map['rules']))
+          : [],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'bethakNo': bethakNo,
-      'title': title,
-      'isFavorite': isFavorite,
+      'number': number,
+      'name': name,
+      'address': address,
+      // Encode lists/maps as JSON strings for SQLite
+      'contacts': jsonEncode(contacts),
+      'mahatmy': mahatmy,
+      'directions': directions,
+      'rules': jsonEncode(rules),
     };
   }
 }
