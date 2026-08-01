91e36f2fd40b6f7fd60e7299520527062c713b85
sujalR978 <srashiya955@rku.ac.in>
2026-08-01 18:58:28 +0530

    #

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@ import 'package:provider/provider.dart';
 import 'package:pushtidham/Provider/theme_manager.dart';
 import 'package:pushtidham/l10n/app_localizations.dart';
 import 'package:pushtidham/screen/Splesh%20Screen/spleshScreen.dart';
-import 'package:pushtidham/theme/app_theme.dart';
+import 'package:pushtidham/theme/app_theme.dart'; // Ensure this exports AppThemes
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    ChangeNotifierProvider(create: (_) => ThemeManager(), child: const MyApp()),
+    ChangeNotifierProvider(
+      create: (_) => ThemeManager(),
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -22,27 +26,37 @@ class MyApp extends StatelessWidget {
     ThemeData darkTarget;
     ThemeMode activeSystemMode;
 
-    switch (themeManager.CurrentTheme) {
-      case AppThemeMode.FollowSystem:
-        lightTarget = AppThemes.dayTheme;
-        darkTarget = AppThemes.nightTheme;
-        activeSystemMode = ThemeMode.system;
+    switch (themeManager.currentTheme) {
+      case AppThemeMode.temple:
+        lightTarget = AppThemes.templeTheme;
+        darkTarget = AppThemes.templeTheme;
+        activeSystemMode = ThemeMode.light;
         break;
-      case AppThemeMode.night:
-        lightTarget = AppThemes.nightTheme;
-        darkTarget = AppThemes.nightTheme;
-        activeSystemMode = ThemeMode.dark;
-        break;
+
       case AppThemeMode.mandir:
         lightTarget = AppThemes.mandirTheme;
         darkTarget = AppThemes.mandirTheme;
         activeSystemMode = ThemeMode.light;
         break;
+
+      case AppThemeMode.night:
+        lightTarget = AppThemes.nightTheme;
+        darkTarget = AppThemes.nightTheme;
+        activeSystemMode = ThemeMode.dark;
+        break;
+
       case AppThemeMode.day:
-      default:
         lightTarget = AppThemes.dayTheme;
         darkTarget = AppThemes.dayTheme;
         activeSystemMode = ThemeMode.light;
+        break;
+
+      case AppThemeMode.followSystem:
+      default:
+        // Defaults to Temple Gold for Light Mode and Night Theme for Dark Mode
+        lightTarget = AppThemes.templeTheme;
+        darkTarget = AppThemes.nightTheme;
+        activeSystemMode = ThemeMode.system;
         break;
     }
 
@@ -51,10 +65,10 @@ class MyApp extends StatelessWidget {
       theme: lightTarget,
       darkTheme: darkTarget,
       themeMode: activeSystemMode,
-      
+
       // Dynamic locale powered by ThemeManager & SharedPreferences
       locale: themeManager.appLocale,
-      
+
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: const Spleshscreen(),
